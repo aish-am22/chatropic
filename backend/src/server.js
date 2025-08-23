1a1ded746f243137fc441bc77e714a009c266348
Aishwarya <swarnaaishwarya17@gmail.com>
2025-08-23 19:36:53 +0530

    fixed server.js imports

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,28 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
-import {clerkMidleware} from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
 import { functions, inngest } from './config/inngest.js';
 import { serve } from 'inngest';
 
 const app = express();
 
 app.use(express.json());
-app.use(clerkMidleware());
+app.use(clerkMiddleware());
 
-app.use("api/inngest", serve ({client: inngest, functions}))
+// fixed: added slash
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.get ("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 const startServer = async () => {
     try {
         await connectDB();
-        if (ENV.NODE_ENV!=="production"){
+
+        // don’t run express.listen in Vercel
+        if (ENV.NODE_ENV !== "production") {
             app.listen(ENV.PORT, () => {
                 console.log(`Server running on port ${ENV.PORT}`);
             });
