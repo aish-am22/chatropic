34b7924063379ac01fb3702453a4cca7ecec78a5
Aishwarya <swarnaaishwarya17@gmail.com>
2025-08-23 19:13:50 +0530

    small fixes in inngest.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,35 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
+import {clerkMidleware} from '@clerk/express'
+import { functions, inngest } from './config/inngest.js';
+import { serve } from 'inngest/serve';
 
 const app = express();
 
+app.use(express.json());
+app.use(clerkMidleware());
+
+app.use("api/inngest", serve ({client: inngest, functions}))
 
 app.get ("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(ENV.PORT, ()=> 
-    {console.log("Server running on port " , ENV.PORT)
-        connectDB() ;    
-    });
+const startServer = async () => {
+    try {
+        await connectDB();
+        if (ENV.NODE_ENV!=="production"){
+            app.listen(ENV.PORT, () => {
+                console.log(`Server running on port ${ENV.PORT}`);
+            });
+        }
+    } catch (error) {
+        console.error("Error starting the server", error);
+        process.exit(1);
+    }
+};
+
+startServer();
+
+export default app;
